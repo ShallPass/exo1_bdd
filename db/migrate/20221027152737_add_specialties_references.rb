235a68f7f73c2b32825916adf29d0c219f13d96f
ShallPass <impactaxie@gmail.com>
2022-10-27 21:46:12 +0200

    Commit du code

---
+++ db/migrate/20221027152737_add_specialties_references.rb
@@ -0,0 +1,6 @@
+class AddSpecialtiesReferences < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :doctors, :specialty, foreign_key: true
+    add_reference :specialties, :doctor, foreign_key: true
+  end
+end
